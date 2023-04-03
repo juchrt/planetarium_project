6f7deb585d2050404f870ed66036f50b5fa31a0e
juliechrt <mailchartier.julie@gmail.com>
2023-04-03 17:44:27 +0200

    fix: add last file adding axios in project

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import axios from 'axios';
+import VueAxios from "vue-axios";
 
 import "./assets/main.css";
 
 const app = createApp(App);
 
-app.use(router);
+app.use(router, axios, VueAxios);
 
 app.mount("#app");
